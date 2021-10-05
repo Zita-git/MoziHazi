81459939a2ab0d93c47dd4af8ca50197eaa84825
Zita <ambrus.zita99@gmail.com>
2021-10-05 15:31:33 +0200

    js alap hozzaadhoz

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ class Film{
         this.kiadasiEv=kiadasiEv;
 
     }
+}
+let adatok=[];
+
+function hozzaad(){
+    let neve=document.getElementById('nev').value;
+    let rendezoje=document.getElementById('rendezo').value;
+    let kiadEve=document.getElementById('kiadasiEv').value;
+
+    let egyFilmAdat= [neve, rendezoje, kiadEve];
+
+    adatok.push(egyFilmAdat);
+
+    document.getElementById('nev').innerHTML="";
+    document.getElementById('rendezo').innerHTML="";
+    document.getElementById('kiadasiEv').innerHTML="";
 
 }
-let{adatok}=[];
+
+document.getElementById('hozzaad').addEventListener('click',hozzaad);
